fda6424226daef50917cf3ac35ca0f44eb2a9b96
Jens Lody <jens@jenslody.de>
2017-02-04 20:32:52 +0100

    Add mbar as pressure unit, based on pr #171 , thanks dafoxia

--- src/extension.js
+++ src/extension.js
@@ -124,7 +124,8 @@ const WeatherPressureUnits = {
     AT: 6,
     TORR: 7,
     PSI: 8,
-    MMHG: 9
+    MMHG: 9,
+    MBAR: 10
 };
 
 const WeatherPosition = {
@@ -1277,6 +1278,11 @@ const OpenweatherMenuButton = new Lang.Class({
             case WeatherPressureUnits.MMHG:
                 pressure = (pressure * 0.750061683).toFixed(this._decimal_places);
                 pressure_unit = _("mmHg");
+                break;
+
+            case WeatherPressureUnits.MBAR:
+                pressure = pressure.toFixed(this._decimal_places);
+                pressure_unit = _("mbar");
                 break;
         }
         return parseFloat(pressure).toLocaleString() + ' ' + pressure_unit;
