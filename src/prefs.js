penguin-teal/gnome-openweather

commit 21f82996a568b375bd4f8c5236361d77a29ecdcf

Do not use deprecated popup-function, fixes #206. Instead of logging a warning, that the GtkMenu.popup function is deprecated since 3.22 and should not be used in new code, they just disabled it (in 3.22) and break code.

src/prefs.js
@@ -30,6 +30,7 @@
  *
  */
 const Gtk = imports.gi.Gtk;
+const Gdk = imports.gi.Gdk;
 const GObject = imports.gi.GObject;
 const Gettext = imports.gettext.domain('gnome-shell-extension-openweather');
 const _ = Gettext.gettext;
@@ -185,7 +186,7 @@ const WeatherPrefsWidget = new GObject.Class({
 
             this.searchMenu.append(item);
             this.searchMenu.show_all();
-            this.searchMenu.popup(null, null, Lang.bind(this, this.placeSearchMenu), 0, this.searchName);
+            this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
 
             if (this.geolocation_provider == GeolocationProvider.OPENSTREETMAPS) {
                 let params = {
@@ -225,7 +226,7 @@ const WeatherPrefsWidget = new GObject.Class({
                         }
                     }
                     this.searchMenu.show_all();
-                    this.searchMenu.popup(null, null, Lang.bind(this, this.placeSearchMenu), 0, this.searchName);
+                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
                     return 0;
                 }));
             } else if (this.geolocation_provider == GeolocationProvider.MAPQUEST) {
@@ -240,7 +241,7 @@ const WeatherPrefsWidget = new GObject.Class({
                     });
                     this.searchMenu.append(item);
                     this.searchMenu.show_all();
-                    this.searchMenu.popup(null, null, Lang.bind(this, this.placeSearchMenu), 0, this.searchName);
+                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
                     return 0;
                 }
                 let params = {
@@ -289,7 +290,7 @@ const WeatherPrefsWidget = new GObject.Class({
                         }
                     }
                     this.searchMenu.show_all();
-                    this.searchMenu.popup(null, null, Lang.bind(this, this.placeSearchMenu), 0, this.searchName);
+                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
                     return 0;
                 }));
             } else if (this.geolocation_provider == GeolocationProvider.GEOCODE) {
@@ -338,7 +339,7 @@ const WeatherPrefsWidget = new GObject.Class({
                     }
 
                     this.searchMenu.show_all();
-                    this.searchMenu.popup(null, null, Lang.bind(this, this.placeSearchMenu), 0, this.searchName);
+                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
                     return 0;
                 }));
             }
@@ -421,12 +422,6 @@ const WeatherPrefsWidget = new GObject.Class({
 
     onActivateItem: function() {
         this.searchName.set_text(arguments[0].get_label());
-    },
-
-    placeSearchMenu: function() {
-        let[gx, gy, gw, gh] = this.searchName.get_window().get_geometry();
-        let[px, py] = this.searchName.get_window().get_position();
-        return [gx + px, gy + py + this.searchName.get_allocated_height()];
     },
 
     clearSearchMenu: function() {

commit cac94f20192cc41a13edad68536b32b600b4f992

Restore search-results popup on gtk3 < 3.22, broken in commit 21f8299.

src/prefs.js
@@ -185,8 +185,7 @@ const WeatherPrefsWidget = new GObject.Class({
             }
 
             this.searchMenu.append(item);
-            this.searchMenu.show_all();
-            this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
+            this.showSearchMenu();
 
             if (this.geolocation_provider == GeolocationProvider.OPENSTREETMAPS) {
                 let params = {
@@ -225,8 +224,7 @@ const WeatherPrefsWidget = new GObject.Class({
                             }
                         }
                     }
-                    this.searchMenu.show_all();
-                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
+                    this.showSearchMenu();
                     return 0;
                 }));
             } else if (this.geolocation_provider == GeolocationProvider.MAPQUEST) {
@@ -240,8 +238,7 @@ const WeatherPrefsWidget = new GObject.Class({
                         label: "Please visit https://developer.mapquest.com/ ."
                     });
                     this.searchMenu.append(item);
-                    this.searchMenu.show_all();
-                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
+                    this.showSearchMenu();
                     return 0;
                 }
                 let params = {
@@ -289,8 +286,7 @@ const WeatherPrefsWidget = new GObject.Class({
                             }
                         }
                     }
-                    this.searchMenu.show_all();
-                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
+                    this.showSearchMenu();
                     return 0;
                 }));
             } else if (this.geolocation_provider == GeolocationProvider.GEOCODE) {
@@ -338,8 +334,7 @@ const WeatherPrefsWidget = new GObject.Class({
                         }
                     }
 
-                    this.searchMenu.show_all();
-                    this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
+                    this.showSearchMenu();
                     return 0;
                 }));
             }
@@ -422,6 +417,23 @@ const WeatherPrefsWidget = new GObject.Class({
 
     onActivateItem: function() {
         this.searchName.set_text(arguments[0].get_label());
+    },
+
+    showSearchMenu: function() {
+        this.searchMenu.show_all();
+        if (versionAtLeast('3.22', Config.PACKAGE_VERSION)) {
+            this.searchMenu.popup_at_widget(this.searchName, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
+        }
+        else
+        {
+            this.searchMenu.popup(null, null, Lang.bind(this, this.placeSearchMenu), 0, this.searchName);
+        }
+    },
+
+    placeSearchMenu: function() {
+        let[gx, gy, gw, gh] = this.searchName.get_window().get_geometry();
+        let[px, py] = this.searchName.get_window().get_position();
+        return [gx + px, gy + py + this.searchName.get_allocated_height()];
     },
 
     clearSearchMenu: function() {