ee99918d72d6bdd387818be69adf5f8ff2236806
Jens Lody <jens@jenslody.de>
2020-05-31 13:29:53 +0200

    (Temporary?) hotfix for broken certification-chain of openweathermap.org.
    Signed-off-by: Jens Lody <jens@jenslody.de>

--- src/extension.js
+++ src/extension.js
@@ -1115,6 +1115,9 @@ class OpenweatherMenuButton extends PanelMenu.Button {
         if (_httpSession === undefined) {
             _httpSession = new Soup.Session();
             _httpSession.user_agent = this.user_agent;
+            if (this._weather_provider == WeatherProvider.OPENWEATHERMAP) {
+                _httpSession.ssl_strict = false;
+            }
         } else {
             // abort previous requests.
             _httpSession.abort();
